16d45eab3990dbaac3be1e5240bf659a37d32281
DarjanCrncic <darjancr@gmail.com>
2021-09-22 22:04:27 +0200

    Minor changes to statistics page.

--- src/components/quiz/WinRateMeter.js
+++ src/components/quiz/WinRateMeter.js
@@ -1,4 +1,4 @@
-import { makeStyles, Paper, Typography } from "@material-ui/core";
+import { Grid, makeStyles, Paper, Typography } from "@material-ui/core";
 import React from "react";
 import { pieColors } from "./pieColors";
 
@@ -10,7 +10,12 @@ const useStyles = makeStyles({
   root: {
     padding: 5,
     margin: "5px auto 5px auto",
+  },
+  textHolderLeft: {
     textAlign: "left",
+  },
+  textHolderRight: {
+    textAlign: "right",
   },
   barHolder: {
     border: "1px solid",
@@ -25,10 +30,21 @@ const WinRateMeter = (props) => {
     <Paper
       className={classes.root}
       style={{
-        transform: props.selectedCategory === props.index ? "scale(1.05)" : "scale(1)",
+        transform:
+          props.selectedCategory === props.index ? "scale(1.05)" : "scale(1)",
       }}
     >
-      <Typography>{props.data.label}</Typography>
+      <Grid container>
+        <Grid item xs={12} sm={9} className={classes.textHolderLeft}>
+          <Typography component="span">{props.data.label}</Typography>
+        </Grid>
+        <Grid item xs={12} sm={3} className={classes.textHolderRight}>
+          <Typography component="span">
+            {Math.round(props.data.avg_score * 100) + "%"}
+          </Typography>
+        </Grid>
+      </Grid>
+
       <div className={classes.barHolder}>
         <div
           className={classes.bar}
