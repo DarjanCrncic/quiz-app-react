e13b70a3cc9af5a55d952c4f3131acfe700354c1
DarjanCrncic <darjancr@gmail.com>
2021-09-29 21:07:06 +0200

    Fixed home page look.

--- src/components/various/HomePageIntro.js
+++ src/components/various/HomePageIntro.js
@@ -1,29 +1,56 @@
-import { makeStyles, Typography } from "@material-ui/core";
+import { Grid, makeStyles, Typography } from "@material-ui/core";
 import React from "react";
 
 const useStyles = makeStyles((theme) => ({
   introText: {
-    textAlign: "center",
-    margin: "100px 0px 100px 0px",
+    textAlign: "left",
     backgroundColor: theme.palette.primary.light,
     fontWeight: 900,
-    lineHeight: "3rem",
-    transform: "scaleY(1.1)",
-    color: theme.palette.primary.contrastText,
-    borderTop: "10px solid " + theme.palette.primary.main,
-    borderBottom: "10px solid " + theme.palette.primary.main,
-    padding: 40,
-    paddingBottom: 50
+    lineHeight: "5.5rem",
+    color: "white",
+    padding: "40px 0px 30px 100px",
+    [theme.breakpoints.down('md')]: {
+      textAlign: "center",
+      padding: 20
+    },
   },
-  borderTop: {
-    marginTop: "5%",
-    backgroundColor: theme.palette.primary.main,
-    height: 20,
+  container: {
+    margin: "100px 0px 100px 0px",
   },
-  borderBottom: {
-    backgroundColor: theme.palette.primary.main,
-    height: 5,
-    marginBottom: 25
+  whiteDivHolder: {
+    backgroundColor: theme.palette.primary.light,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "end",
+    paddingTop: 10,
+    paddingBottom: 10,
+    [theme.breakpoints.down('sm')]: {
+      display: "none"
+    },
+  },
+  WhiteDiv: {
+    marginTop: "auto",
+    marginBottom: "auto",
+    backgroundColor: "white",
+    height: "3rem",
+  },
+  innerTriangle: {
+    borderTopColor: theme.palette.primary.light,
+    borderRightColor: "white",
+    borderWidth: "3rem 2rem 0 0",
+    borderStyle: "solid",
+    position: "relative",
+    width: 0,
+    height: 0
+  },
+  divTop: {
+    width: "calc(100% - 4rem)",
+  },
+  divMiddle: {
+    width: "calc(100% - 2rem)",
+  },
+  divBottom: {
+    width: "100%",
   },
 }));
 
@@ -31,9 +58,18 @@ const HomePageIntro = (props) => {
   const classes = useStyles();
   return (
     <div>
-      <Typography variant="h3" className={classes.introText}>
-        {props.children}
-      </Typography>
+      <Grid container className={classes.container}>
+          <Grid item sm={12} md={8}>
+            <Typography variant="h3" className={classes.introText}>
+              {props.children}
+            </Typography>
+          </Grid>
+          <Grid item sm={0} md={4} className={classes.whiteDivHolder}>
+            <div className={classes.divTop + " " + classes.WhiteDiv}><div className={classes.innerTriangle}></div></div>
+            <div className={classes.divMiddle + " " + classes.WhiteDiv}><div className={classes.innerTriangle}></div></div>
+            <div className={classes.divBottom + " " + classes.WhiteDiv}><div className={classes.innerTriangle}></div></div>
+          </Grid>
+      </Grid>
     </div>
   );
 };
