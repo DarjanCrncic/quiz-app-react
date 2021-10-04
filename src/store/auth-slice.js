c265e56d43353ea5a2f4c13343ebe79e07347790
Darjan Crnčić <darjancr@gmail.com>
2021-10-04 16:11:31 +0200

    Try.

--- src/store/auth-slice.js
+++ src/store/auth-slice.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export const checkLogin = createAsyncThunk("auth/checkLogin", async (data) => {
   console.log("checking logging in...");
-  return axios.get("/api/users/authenticated").then((response) => response.data);
+  return axios.get(process.env.REACT_APP_API_URL + "/api/users/authenticated").then((response) => response.data);
 });
 
 const authSlice = createSlice({
